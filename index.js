dbf4aecb20d5e4898f7781d72b7fb569d28adc29
Aws-star <75694518+Aws-star@users.noreply.github.com>
2023-03-08 23:19:00 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
+let sum = 0;
 function sumItems(array) {
   // Sum all the numbers in the array
+  for (let i = 0; i < array.length; i ++) {
+    if (Array.isArray(array[i])) {
+      sumItems(array[i]);
+    } else { 
+      sum += array[i];
+
+    }
+  }
+  return sum;
 }
+
 
 module.exports = sumItems;
